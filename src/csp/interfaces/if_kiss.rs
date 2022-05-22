d79d53411a43e26c451f840bb88ab154aa7e4b32
mariusmonton <marius.monton@gmail.com>
2022-05-22 17:33:39 +0200

    KISS protocol for RX

--- src/csp/interfaces/if_kiss.rs
+++ src/csp/interfaces/if_kiss.rs
@@ -7,6 +7,12 @@ use crate::csp::interface::*;
 use crate::csp::types::*;
 use crate::csp::utils::*;
 
+const FEND:u8 = 0xC0;
+const FESC:u8 = 0xDB;
+const TFEND:u8 = 0xDC;
+const TFESC:u8 = 0xDD;
+const TNC_DATA:u8 = 0x00;
+
 enum CspKissMode {
     KissModeNotStarted, // No start detected
     KissModeStarted,    // Started on a KISS frame
@@ -16,7 +22,7 @@ enum CspKissMode {
 
 pub struct KissIntfData {
     intf: CspIface,
-    pub max_rx_length: u32,
+    pub max_rx_length: usize,
     pub rx_length: u32,
     pub rx_first: bool,
     pub port: Option<Box<dyn SerialPort>>,
@@ -50,7 +56,7 @@ impl KissIntfData {
         println!("Kiss TX {} {}", self.intf.name, packet.length);
     
         let length = csp_crc32_append(& mut packet.data, packet.length);
-        let kiss_buf = kiss_process(&packet.data, length);
+        let kiss_buf = kiss_process_tx(&packet.data, length);
         let mem_buff = Bytes::from(kiss_buf);
      
         match &self.port {
@@ -68,7 +74,7 @@ impl KissIntfData {
 pub fn csp_kiss_rx (interface: &mut KissIntfData,
     packet: &mut crate::csp::types::CspPacket) -> Result<(), io::Error> {
 
-        let mut serial_buf: Vec<u8> = vec![0; 255];
+        let mut serial_buf: Vec<u8> = vec![0; interface.max_rx_length];
         match &interface.port {
 
             //TODO: better error management
@@ -79,8 +85,9 @@ pub fn csp_kiss_rx (interface: &mut KissIntfData,
                     let r = cl.read(serial_buf.as_mut_slice());
                     match r {
                         Ok(t) => {
-                            packet.data = serial_buf;
-                            packet.length = t;
+                            kiss_process_rx (serial_buf, t, packet, interface).unwrap();
+                            //packet.data = serial_buf;
+                            //packet.length = t;
                             return Ok(());
                         },
                         Err(ref e) if e.kind() == io::ErrorKind::TimedOut => (),
@@ -99,13 +106,7 @@ impl crate::csp::interface::NextHop for KissIntfData {
     }
 }
 
-fn kiss_process(data: &[u8], len: usize) -> Vec<u8> {
-
-    const FEND:u8 = 0xC0;
-    const FESC:u8 = 0xDB;
-    const TFEND:u8 = 0xDC;
-    const TFESC:u8 = 0xDD;
-    const TNC_DATA:u8 = 0x00;
+fn kiss_process_tx(data: &[u8], len: usize) -> Vec<u8> {
 
     let mut res = Vec::new();
 
@@ -131,6 +132,78 @@ fn kiss_process(data: &[u8], len: usize) -> Vec<u8> {
     
     return res;
 }
+
+fn kiss_process_rx(data: Vec<u8>
+    , len: usize
+    , packet: &mut crate::csp::types::CspPacket
+    , intf: &mut KissIntfData,
+    ) -> Result<(), io::Error> {
+    let mut n = 0;
+    while n < len {
+        let inputbyte = data[n];
+        n += 1;
+        match intf.rx_mode {
+            CspKissMode::KissModeNotStarted => {
+                if inputbyte != FEND {
+                    break;
+                }
+                //csp_id_setup_rx();
+                if packet.length > intf.max_rx_length {
+                    intf.rx_mode = CspKissMode::KissModeSkipFrame;
+                }
+                intf.rx_first = true;
+                intf.rx_length = 0;
+                intf.rx_mode = CspKissMode::KissModeStarted;
+            },
+            CspKissMode::KissModeStarted => {
+                if inputbyte == FESC {
+                    intf.rx_mode = CspKissMode::KissModeEscaped;
+                    continue;
+                }
+
+                if inputbyte == FEND {
+                    packet.length = len;
+                    // if csp_id_strip <0 error
+                    
+                    // intf.frame += 1;
+                    // validate crc
+                    // send to CSP task server using qfifo (?)
+
+                    intf.rx_mode = CspKissMode::KissModeNotStarted;
+                    continue;
+                }
+
+                if intf.rx_first {
+                    intf.rx_first = false;
+                    continue;
+                }
+
+                packet.data.push(inputbyte);
+            },
+            CspKissMode::KissModeEscaped => {
+                if inputbyte == TFESC {
+                    packet.data.push(FESC);
+                }
+
+                if inputbyte == TFEND {
+                    packet.data.push(FEND);
+                }
+                intf.rx_mode = CspKissMode::KissModeStarted;
+            }
+
+            CspKissMode::KissModeSkipFrame => {
+                if inputbyte == FEND {
+                    intf.rx_mode = CspKissMode::KissModeNotStarted;
+                }
+            }
+
+
+        };
+
+    }
+    Ok(())
+}
+
 
 #[cfg(test)]
 mod tests {
@@ -204,8 +277,13 @@ mod tests {
         assert! (result.is_ok());
      }
 
-
-     #[test]
+     
+    /// In linux, use the command
+    /// ``` > socat -d -d pty,raw,echo=0 pty,raw,echo=0 ```
+    /// it will printout what ports to use for the test
+    /// One port is used here, use the other in other console with the following command
+    /// ``` > echo "helloWorld" > /dev/pty/X
+    #[test]
     fn csp_uart_rx_test() {
         let my_csp_id = CspId {
              pri: 2,
@@ -220,7 +298,7 @@ mod tests {
              frame_begin: [0; 4],
              length: 25,
              id: my_csp_id,
-             data: vec![65; 256],
+             data: vec![65; 1],
          };
  
          
@@ -258,6 +336,59 @@ mod tests {
         usart_open(&mut test_int, uart_config, "/dev/pts/5".to_string()).unwrap();
         let result = csp_kiss_rx(&mut test_int, &mut pkt);
         println!("UART RX: {:#?}", pkt.data);
+        println!("Packet len: {}", pkt.length);
         assert! (result.is_ok());
+     }
+
+     #[test]
+     fn csp_kiss_process_rx_test () {
+        let my_csp_id = CspId {
+            pri: 2,
+            flags: 1,
+            src: 5,
+            dst: 12,
+            dport: 23,
+            sport: 99,
+        };
+         
+        let mut pkt = CspPacket {
+            frame_begin: [0; 4],
+            length: 25,
+            id: my_csp_id,
+            data: Vec::new(),
+        };
+
+        
+        let mut test_int = KissIntfData {
+            intf: CspIface {
+                addr: 12,
+                netmask: 5,
+                name: "KISS".to_string(),
+                mtu: 7,
+                split_horizon_off: 1,
+                tx: 0,
+                rx: 0,
+                tx_error: 0,
+                rx_error: 0,
+                drop: 0,
+                autherr: 0,
+                frame: 0,
+                txbytes: 0,
+                rxbytes: 0,
+                irq: 0,
+            },
+            max_rx_length: 256,
+            rx_mode: CspKissMode::KissModeNotStarted,
+            rx_length: 0,
+            rx_first: false,
+            port: None,
+        };
+
+        //let data = vec![0xC0, 0x00, 0x12, 0x34, 0x56, 0x78];
+        //let data = vec![0xC0, 0x00, 0x54, 0xDB, 0xDC, 0xDB, 0xDD, 0x53, 0x54, 0xC0];
+        let data = vec![0xC0, 0x00, 0xDB, 0xDC, 0xDB, 0xDD];
+        kiss_process_rx (data, 6, &mut pkt, &mut test_int).unwrap();
+        println!("len: {} Data: {:#02x?}", pkt.length, pkt.data);
+        assert_eq!(pkt.data, vec![0xC0,0xDB]);
      }
 }
